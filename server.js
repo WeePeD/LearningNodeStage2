e8f9cc5c7931cef88814920c3d685d2a73c8b201
WeePeD <87631731+WeePeD@users.noreply.github.com>
2022-09-10 01:16:57 +0700

    fixing 1

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const bodyParser = require('body-parser');
 
 const absolutePath  = __dirname + "/views/index.html";
 
-app.use('/',express.static(__dirname + '/public'));
+app.use('/public',express.static(__dirname + '/public'));
 
 app.use((req,res,next)=>{
   console.log(req.method +" "+req.path+" - "+req.ip);
