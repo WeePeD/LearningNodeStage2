3b9c1acfb35b9406c057f69a57b4e864d5aa8a5d
WeePeD <87631731+WeePeD@users.noreply.github.com>
2022-09-09 23:52:36 +0700

    lession 11 up3

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const testPath = __dirname + "/public";
 
 
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 console.log("TestPath: "+ testPath);
 
 
