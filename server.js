d3438427372d5938100e931cee6141dbcb444193
WeePeD <87631731+WeePeD@users.noreply.github.com>
2022-09-10 01:23:26 +0700

    f2

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const bodyParser = require('body-parser');
 
 
 app.use('/public',express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use((req,res,next)=>{
   console.log(req.method +" "+req.path+" - "+req.ip);
