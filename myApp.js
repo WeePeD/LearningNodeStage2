b7827f28ec4192d6c93bc30178a814ba834e2722
WeePeD <87631731+WeePeD@users.noreply.github.com>
2022-09-09 23:58:42 +0700

    l

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const absolutePath2 = __dirname + "/public";
 
 
+app.use(express.static(absolutePath2));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
